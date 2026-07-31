8358d0eed39765b2629a35ff4bc817c080be58ca
Paulo Daniel <pda.ambrosio@gmail.com>
2026-07-31 19:48:46 -0300

    feat: new methods in agents package

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,6 +1,9 @@
+from agents.agent import DiagnosticAgent, DiagnosticResult
 from agents.tools import build_tools, execute_tool_command
 
 __all__ = [
+    "DiagnosticAgent",
+    "DiagnosticResult",
     "build_tools",
     "execute_tool_command",
 ]
